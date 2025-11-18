d9a7f3daba9b87be9363964b09784d8de6a526f4
Astro-Coco <colinc131@gmail.com>
2025-11-18 10:41:05 -0500

    params

---
+++ data_structures.py
@@ -0,0 +1,11 @@
+class Params:
+    def __init__(self, H, mu, rho, T_in, T_w, cp, k, L):
+        self.H = H
+        self.mu = mu
+        self.rho = rho
+        self.T_in = T_in
+        self.T_w = T_w
+        self.cp = cp
+        self.k = k
+        self.L = L
+
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from data_structures import *
 from helpers import *
 
+params = Params(
+    H = 0.01,
+    mu = 0.001,
+    rho = 1000,
+    T_in = 298,
+    T_w = 373,
+    cp = 4186,
+    k = 60,
+    L = 1.0
+)
 
+U = 0.1 # Vitesse d'entrée
 
 
 def main():
