daa4a5ead07cfcab4589a5365449075890270b48
Astro-Coco <colinc131@gmail.com>
2025-11-18 10:17:29 -0500

    équations de def des vitesses

--- helpers.py
+++ helpers.py
@@ -1,1 +1,12 @@
-pass
+import math
+def vitesse_developpée(x,y, params):
+    vy = 0
+    vx = 3/2*params.U*(1 - (2*y/params.H)**2)
+    return vx, vy
+
+
+def vitesse_developpement(x,y, params):
+    vy = 0
+    delta_x = math.sqrt(params.mu*x/params.rho/params.U)
+    vx = params.U*(1 - math.exp(-y/delta_x))
+    return vx, vy
