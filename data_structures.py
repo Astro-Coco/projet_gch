235a49c8982afe8dc0698dd18c6f644f2daa251d
PushAndWill <willpg2004@gmail.com>
2025-11-26 17:04:49 -0500

    willou pu bcp de pile

--- data_structures.py
+++ data_structures.py
@@ -16,11 +16,13 @@ class Params:
 
 
 class Results:
-    def __init__(self, x, y, T, U,y_limite_T=None,y_limite_U=None):
+    def __init__(self, x, y, T, U,y_limite_T=None,y_limite_U=None,y_limite_T_symm=None,y_limite_U_symm=None):
         self.x_mat = x
         self.y_mat = y
         self.T_mat = T
         self.U_mat = U
         self.y_limite_T=y_limite_T
         self.y_limite_U=y_limite_U
+        self.y_limite_T_symm=y_limite_T_symm
+        self.y_limite_U_symm=y_limite_U_symm
 
